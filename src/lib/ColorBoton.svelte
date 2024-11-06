a9fe4355f059dba05394e9f4bf809b515fa7427c
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-11-06 09:40:17 +0100

    Boton que cambia de color

---
+++ src/lib/ColorBoton.svelte
@@ -0,0 +1,24 @@
+<script>
+
+function changeColor(){
+    let boton = document.getElementById('boton');
+    let contador = 0;
+    let colores = ["red", "green", "purple", "orange"];
+
+    if(contador > colores.length){
+        contador = 0;
+    }
+    else{
+        contador++;
+        document.getElementById('boton').style.backgroundColor = colores[contador];
+    }
+
+boton.addEventListener('click', changeColor);
+
+}
+
+</script>
+
+<div class="box">
+    <button id="boton" on:click={changeColor}>Cambiar color</button>
+</div>
